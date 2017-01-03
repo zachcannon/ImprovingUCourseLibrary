ed21dc9d1280a5b6fb4c52a2a344ce0b99c412d7
Michael L Perry <michael@qedcode.com>
2017-01-02 23:39:46 -0600

    Use jinaga.app to set up distributor and authorization.

--- config/config.example.js
+++ config/config.example.js
@@ -1,9 +1,12 @@
 module.exports = {
+    baseUrl: 'http://localhost:8080',
     port: 8080,
     sessionSecret: 'secretString',
     ad: {
         realm: 'http://mysite.com',
-        logoutUrl: 'http://mysite.com',
+        loginUrl: '/login',
+        redirectUrl: '/login/callback',
+        logoutUrl: '/logout',
         identityProviderUrl: 'https://login.windows.net/my-tenant-id/wsfed',
         identityMetadata: 'https://login.windows.net/my-tenant-id/federationmetadata/2007-06/federationmetadata.xml'
     },
