011b04b85567b7ce08c124f834741aaf4f4d778a
Michael L Perry <michael@qedcode.com>
2017-08-27 20:37:45 -0500

    Separate course and semester models.

--- src/client/course.ts
+++ src/client/course.ts
@@ -1,21 +1,3 @@
-class User {
-    public type: string;
-    public publicKey: string;
-}
-
-class Company {
-    public type: string;
-    public name: string;
-    public from: User;
-}
-
-class Semester {
-    public type: string;
-    public name: string;
-    public _in: Company;
-    public from: User;
-}
-
 class Catalog {
     public type: string;
     public office?: string;
@@ -45,23 +27,6 @@ class CourseTitle {
     public prior?: CourseTitle[];
     public _in?: Catalog;
 }
-
-const semesterName = "Fall 2017";
-const owner : User = {
-    type: "Jinaga.User",
-    publicKey: "-----BEGIN RSA PUBLIC KEY-----\nMIGJAoGBAIBsKomutukULWw2zoTW2ECMrM8VmD2xvfpl3R4qh1whzuXV+A4EfRKMb/UAjEfw\n5nBmWvcObGyYUgygKrlNeOhf3MnDj706rej6ln9cKGL++ZNsJgJsogaAtmkPihWVGi908fdP\nLQrWTF5be0b/ZP258Zs3CTpcRTpTvhzS5TC1AgMBAAE=\n-----END RSA PUBLIC KEY-----\n"
-};
-const company : Company = {
-    type: "ImprovingU.Company",
-    name: "Improving",
-    from: owner
-};
-const semester : Semester = {
-    type: "ImprovingU.Semester",
-    name: semesterName,
-    _in: company,
-    from: owner
-};
 
 
 // Factory functions
